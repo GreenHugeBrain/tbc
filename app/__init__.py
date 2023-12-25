0e84b82eeefa9bb8ef6a6694cd51b993629edb5d
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2023-12-25 21:05:22 +0400

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from ext import app
-from routes import AddComment, register, login, logout, admindashboard, edit_product, remove_product
+from routes import home, product, Feedback, register, login, logout, admindashboard, edit_product, remove_product
